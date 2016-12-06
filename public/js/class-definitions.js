13d935a882ac9697a593624e43141a87c80534e5
Edward Gao <egao@sent.com>
2016-12-06 11:08:12 -1000

    step 67

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1100,7 +1100,7 @@ var hexagon = new Shape(6);
 
 // Create 2 boxes
 var catBox = new Box(new Animal("Cat", "male"), true);
-var christmasPresent;
+var christmasPresent = new Box(new Shape(3), false);
 
 // Create 2 doors
 var automaticDoor;
