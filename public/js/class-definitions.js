a849a5327c0a1ef186233109b38e593321995549
Edward Gao <egao@sent.com>
2016-12-06 11:08:23 -1000

    step 68

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1103,7 +1103,7 @@ var catBox = new Box(new Animal("Cat", "male"), true);
 var christmasPresent = new Box(new Shape(3), false);
 
 // Create 2 doors
-var automaticDoor;
+var automaticDoor = new Door(true);
 var bankVault;
 
 // Create 2 shoes
