450d6fbd5673f83cb527093e87c94b255cfdbbff
Edward Gao <egao@sent.com>
2016-12-06 11:08:34 -1000

    step 69

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1104,7 +1104,7 @@ var christmasPresent = new Box(new Shape(3), false);
 
 // Create 2 doors
 var automaticDoor = new Door(true);
-var bankVault;
+var bankVault = new Door(false);
 
 // Create 2 shoes
 var rubySlippers;
